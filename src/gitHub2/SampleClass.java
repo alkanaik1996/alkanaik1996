8528da3094e112366abba470b43bb54de1e61c96
Ragini <Ragini@LAPTOP-VDSJV9K1>
2024-02-16 20:06:36 +0530

    Commit this project

---
+++ src/gitHub2/SampleClass.java
@@ -0,0 +1,10 @@
+package gitHub2;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is new Git Project");
+	}
+
+}
